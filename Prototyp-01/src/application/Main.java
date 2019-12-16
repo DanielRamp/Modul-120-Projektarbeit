57a1a9ae929fdd9c99c2cb1bb78b4772982c4917
Daniel Ramp <12480131+DanielRamp@users.noreply.github.com>
2019-12-16 14:43:20 +0100

    try2
    naja

--- Prototyp-01/src/application/Main.java
+++ Prototyp-01/src/application/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("01_Welcome.fxml"));
+			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("01_Welcome.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("02_Feed.fxml"));
 			Scene scene = new Scene(root,800,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -20,6 +21,10 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void rightClick() {
+		System.out.println("rightClick");
 	}
 	
 	public static void main(String[] args) {
--- Prototyp-01/src/application/SampleController.java
+++ Prototyp-01/src/application/SampleController.java
@@ -1,5 +1,21 @@
 package application;
 
-public class SampleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SampleController implements Initializable {
+	
+	public void rightClick() {
+		System.out.println("rightClick");
+		Main m = new Main();
+		m.main(null);
+	}
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println("initialize executed");
+	}
+
 	
 }
