d34df794a40c9c3612c86b3875fccd9f7369d68b
DanielRamp <daniel.ramp@gibmit.ch>
2020-01-02 22:56:22 +0100

    Update View1App.java
    Stylesheet is already View1.fxml

--- TestJavaFXProject1/src/application/View1App.java
+++ TestJavaFXProject1/src/application/View1App.java
@@ -11,11 +11,11 @@ public class View1App extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View1.fxml"));
+			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View1.fxml"));
 			//Scene scene = new Scene(root,800,600);
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			stage.setScene(scene);
 			stage.show();
 		} catch(Exception e) {
