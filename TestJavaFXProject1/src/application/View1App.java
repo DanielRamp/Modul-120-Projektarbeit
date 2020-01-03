0fbc28a4861159a44ce9c6cc0e88748a1527c93e
DanielRamp <daniel.ramp@gibmit.ch>
2020-01-04 00:56:01 +0100

    Update View1App.java
    Add Helper instead of dirty code

--- TestJavaFXProject1/src/application/View1App.java
+++ TestJavaFXProject1/src/application/View1App.java
@@ -1,15 +1,27 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+/*
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+*/
 
 
 public class View1App extends Application {
+	
 	@Override
 	public void start(Stage stage) {
+		Helper helper = Helper.getInstance();
+		
+		stage = helper.changeView(stage, "View1.fxml");
+		
+		stage.show();
+		
+		/*
+		
 		try {
 			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View1.fxml"));
@@ -20,9 +32,20 @@ public class View1App extends Application {
 			stage.show();
 			stage.setMinHeight(900);
 			stage.setMinWidth(600);
+			
+			
+			stage.setTitle("News");
+			try {
+				Image icon = new Image(getClass().getResourceAsStream("img/newspaper.png"));
+				stage.getIcons().add(icon);
+			} catch (Exception e) {
+				Outputter.err(e.getStackTrace().toString());
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		*/
 	}
 	
 	public static void main(String[] args) {
