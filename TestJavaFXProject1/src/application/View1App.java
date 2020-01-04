d89c2d468eabae882de8bb0edac6eb8333924d46
DanielRamp <daniel.ramp@gibmit.ch>
2020-01-04 01:49:50 +0100

    Update View1App.java
    Cleanup of View1App because of Helper simplification

--- TestJavaFXProject1/src/application/View1App.java
+++ TestJavaFXProject1/src/application/View1App.java
@@ -2,12 +2,6 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-/*
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
-*/
 
 
 public class View1App extends Application {
@@ -19,33 +13,6 @@ public class View1App extends Application {
 		stage = helper.changeView(stage, "View1.fxml");
 		
 		stage.show();
-		
-		/*
-		
-		try {
-			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View1.fxml"));
-			//Scene scene = new Scene(root,800,600);
-			Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			stage.setScene(scene);
-			stage.show();
-			stage.setMinHeight(900);
-			stage.setMinWidth(600);
-			
-			
-			stage.setTitle("News");
-			try {
-				Image icon = new Image(getClass().getResourceAsStream("img/newspaper.png"));
-				stage.getIcons().add(icon);
-			} catch (Exception e) {
-				Outputter.err(e.getStackTrace().toString());
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		*/
 	}
 	
 	public static void main(String[] args) {
