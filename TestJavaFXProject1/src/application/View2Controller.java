7e44cb43840b9d613d0bf048922a5ec61591bd3c
DanielRamp <daniel.ramp@gibmit.ch>
2020-01-04 06:37:21 +0100

    Update View2Controller.java
    Update refreshlstPosts, Remove DummyData from View2Controller and cleaned up

--- TestJavaFXProject1/src/application/View2Controller.java
+++ TestJavaFXProject1/src/application/View2Controller.java
@@ -27,62 +27,47 @@ public class View2Controller implements Initializable {
 	@FXML ListView<String> lstPosts;
 	
 	Data data = Data.getInstance();
+	Helper helper = Helper.getInstance();
 	
 	final ObservableList<String> listItems = FXCollections.observableArrayList("Add Items here");
 	
 	
 	public View2Controller() {
-		if (data.getPostList().isEmpty()) {
-			dummyData();
-		}
+		DummyData.dummyData();
+		refreshlstPosts();
 	}
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		lstPosts.setItems(listItems);
-		refreshlstPosts();
 	}
 
-	public void dummyData() {
-		// creating 3 categories
-		data.getCategories().add(new Category(null, "Kategorie 1", true));
-		data.getCategories().add(new Category(null, "Kategorie 2", true));
-		data.getCategories().add(new Category(null, "Kategorie 3", false));
-		
-		// creating ArrayList with Categories of Post 1
-		ArrayList<Category> post1Categories = new ArrayList<Category>();
-		post1Categories.add(data.getCategories().get(0));
-		post1Categories.add(data.getCategories().get(2));
-		
-		// creating Post1
-		data.addPostList(new Post("icon1", "Titel1", post1Categories, "content1"));
-		
-		// creating Post2
-		ArrayList<Category> post2Categories = new ArrayList<Category>();
-		post2Categories.add(data.getCategories().get(1));
-		Data.getInstance().addPostList(new Post("icon2", "Titel2", post2Categories, "content2"));
-		
-		// creating Post3
-		ArrayList<Category> post3Categories = new ArrayList<Category>();
-		post3Categories.add(data.getCategories().get(2));
-		Data.getInstance().addPostList(new Post("icon3", "Titel3", post3Categories, "content3"));
-	}
-	
 	public void refreshlstPosts() {
 		listItems.clear();
-		for (Post post : Data.getInstance().getPostList()) {
+		for (Post post : data.getPostList()) {
 			if (!post.getIsRead()) {
+				StringBuilder categoriesStringBuilder = new StringBuilder();
+				Boolean isSelected = false;
 				for (Category postCategories : post.getCategories()) {
 					if (postCategories.isSelected()) {
-						listItems.add(post.getContent());
+						isSelected = true;
+					}
+					if (categoriesStringBuilder.toString().isEmpty()) {
+						categoriesStringBuilder.append(postCategories.getName());
+					} else {
+						categoriesStringBuilder.append(", "+postCategories.getName());
 					}
 				}
+				if (isSelected) {
+					listItems.add("Kategorie(n): "+categoriesStringBuilder.toString()+"\nTitel: "+post.getTitle()+"\nInhalt: "+post.getContent()+"");
+				}
+				//listItems.add("Kategorie(n): "+categoriesStringBuilder.toString()+"Titel: "+post.getTitle()+"\nInhalt: "+post.getContent()+"");
+				
 			}
 		}
 	}
 	
 	
 	@FXML public void btnPrevAction() {
-		Helper helper = Helper.getInstance();
 		FXMLLoader load = helper.changeViewLoader(btnPrev, "View1.fxml");
 		
 		// hide lblWelcome on View1 - to recycle View1
@@ -94,20 +79,20 @@ public class View2Controller implements Initializable {
 
 	@FXML public void btnRefreshAction() {
 		refreshlstPosts();
-		// TODO: get data and fill list
 	}
 	
 	
 
 	@FXML public void btnClearPostsAction() {
-		lstPosts.getItems().clear();
+		for (Post post : data.getPostList()) {
+			post.setIsRead(true);
+		}
+		refreshlstPosts();
 	}
 
-
-
+	
 
 	@FXML public void btnNewPostAction() {
-		Helper helper = Helper.getInstance();
 		helper.dialogView(btnPrev, "View3.fxml");
 	}
 
